16dd7ec23d838d4c2d84b06e07fdce675065a906
Denis Renning <denis@devtty.de>
2024-10-04 11:38:21 +0200

    ZipScannerUtils improved

--- src/main/java/org/webcastellum/ZipScannerUtils.java
+++ src/main/java/org/webcastellum/ZipScannerUtils.java
@@ -12,16 +12,8 @@ import java.util.zip.ZipFile;
 
 public final class ZipScannerUtils {
     
-    
     private static final String[] EMPTY = {};
 
-
-    
-    
-    
-    
-    
-    
     public static final String[] extractNameAndCommentStrings(final InputStream input) throws IOException {
         File temp = null;
         try {
@@ -32,39 +24,24 @@ public final class ZipScannerUtils {
         }
     }
     public static final String[] extractNameAndCommentStrings(final File file) throws IOException {
-        ZipFile zipFile = null;
-        try {
-            zipFile = new ZipFile(file, ZipFile.OPEN_READ);
-            final Set/*<String>*/ nameAndComments = new HashSet();
-            ZipEntry entry;
-            String name, comment;
-            for (final Enumeration/*<ZipEntry>*/ entries = zipFile.entries(); entries.hasMoreElements();) {
-                entry = (ZipEntry) entries.nextElement();
-                name = entry.getName();
-                comment = entry.getComment();
-                if (name != null) nameAndComments.add(name);
-                if (comment != null) nameAndComments.add(comment);
-            }
-            return nameAndComments.isEmpty() ? null : (String[]) nameAndComments.toArray( new String[0] );
+        try (ZipFile zipFile = new ZipFile(file, ZipFile.OPEN_READ)) {
+            final Set<String> nameAndComments = new HashSet<>();
+            
+            zipFile.entries().asIterator().forEachRemaining(entry -> {
+                String name = entry.getName();
+                String comment = entry.getComment();
+                if (name != null)
+                    nameAndComments.add(name);
+                if (comment != null)
+                    nameAndComments.add(comment);
+            });
+            
+            return nameAndComments.isEmpty() ? null : (String[]) nameAndComments.toArray(String[]::new);
         } catch (ZipException e) {
             // not a ZIP file - so no ZIP comments
             return EMPTY;
-        } finally {
-            if (zipFile != null) try { zipFile.close(); } catch(IOException ignored) {}
         }
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
     public static final boolean isZipBomb(final InputStream input, final long thresholdTotalSize, final long thresholdFileCount) throws IOException {
         File temp = null;
@@ -78,27 +55,28 @@ public final class ZipScannerUtils {
     public static final boolean isZipBomb(final File file, final long thresholdTotalSize, final long thresholdFileCount) throws IOException {
         if (thresholdTotalSize < 0) throw new IllegalArgumentException("thresholdTotalSize must not be negative");
         if (thresholdFileCount < 0) throw new IllegalArgumentException("thresholdFileCount must not be negative");
-        ZipFile zipFile = null;
-        try {
-            zipFile = new ZipFile(file, ZipFile.OPEN_READ);
+        
+        try(ZipFile zipFile = new ZipFile(file, ZipFile.OPEN_READ)) {
             ZipEntry entry;
-            long countedFiles = 0, countedTotalBytes = 0;
-            for (final Enumeration/*<ZipEntry>*/ entries = zipFile.entries(); entries.hasMoreElements();) {
+            long countedFiles = 0;
+            long countedTotalBytes = 0;
+            
+            for (final Enumeration<? extends ZipEntry> entries = zipFile.entries(); entries.hasMoreElements();) {
                 if (++countedFiles > thresholdFileCount && thresholdFileCount > 0) return true;
                 entry = (ZipEntry) entries.nextElement();
                 final long size = entry.getSize();
                 if (size > 0 && thresholdTotalSize > 0) {
-                    if (size > thresholdTotalSize) return true;
+                    if (size > thresholdTotalSize) 
+                        return true;
                     countedTotalBytes += size;
-                    if (countedTotalBytes > thresholdTotalSize) return true;
+                    if (countedTotalBytes > thresholdTotalSize) 
+                        return true;
                 }
             }
             return false;
         } catch (ZipException e) {
             // not a ZIP file - so no ZIP bomb
             return false;
-        } finally {
-            if (zipFile != null) try { zipFile.close(); } catch(IOException ignored) {}
         }
     }
 
--- src/test/java/org/webcastellum/ZipScannerUtilsTest.java
+++ src/test/java/org/webcastellum/ZipScannerUtilsTest.java
@@ -1,41 +1,55 @@
 package org.webcastellum;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import org.junit.Assert;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ZipScannerUtilsTest {
     
-    public ZipScannerUtilsTest() {
-    }
-
     @Test
-    public void testExtractNameAndCommentStrings_File() throws Exception {
+    public void testExtractNameAndCommentStrings_Stream() throws FileNotFoundException, IOException{
+        InputStream input = new FileInputStream(new File("src/test/resources/test.zip"));
+        String[] str = ZipScannerUtils.extractNameAndCommentStrings(input);
+        Assert.assertArrayEquals(str, new String[]{"test1.txt", "test1comment", "test2.txt"});
+    }
+    
+    @Test
+    public void testExtractNameAndCommentStrings_File() throws IOException{
         File file = new File("src/test/resources/test.zip");
         String[] str = ZipScannerUtils.extractNameAndCommentStrings(file);
         Assert.assertArrayEquals(str, new String[]{"test1.txt", "test1comment", "test2.txt"});
     }
 
-    
-    @Test(expected = IllegalArgumentException.class)
-    public void testIsZipBombWithNegativeTotalSize() throws IOException{
-        ZipScannerUtils.isZipBomb(new File("src/test/resources/test.zip"), -1, 0);
-    }
-    
-    @Test(expected = IllegalArgumentException.class)
-    public void testIsZipBombWithNegativeFileCount() throws IOException{
-        ZipScannerUtils.isZipBomb(new File("src/test/resources/test.zip"), 0, -1);
+    @Test
+    public void testIsZipBombWithNegativeTotalSize(){
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () -> ZipScannerUtils.isZipBomb(new File("src/test/resources/test.zip"), -1, 0));
+        assertEquals("thresholdTotalSize must not be negative", ex.getMessage());
     }
     
     @Test
-    public void testIsZipBomb_3args_2() throws Exception {
+    public void testIsZipBombWithNegativeFileCount(){
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () -> ZipScannerUtils.isZipBomb(new File("src/test/resources/test.zip"), 0, -1));
+        assertEquals("thresholdFileCount must not be negative", ex.getMessage());
+    }
+    
+    @Test
+    public void testIsZipBomb_3args_2() throws IOException{
         File file = new File("src/test/resources/test.zip");
         //test.zip contains 2 files with a 7 bytes
         assertFalse(ZipScannerUtils.isZipBomb(file, 14, 2));
         assertTrue(ZipScannerUtils.isZipBomb(file, 13, 2));
         assertTrue(ZipScannerUtils.isZipBomb(file, 14, 1));
+    }
+    
+    @Test
+    public void testIsZipBomb_Stream() throws FileNotFoundException, IOException{
+        InputStream input = new FileInputStream(new File("src/test/resources/test.zip"));
+        assertFalse(ZipScannerUtils.isZipBomb(input, 14, 2));
     }
     
 }
