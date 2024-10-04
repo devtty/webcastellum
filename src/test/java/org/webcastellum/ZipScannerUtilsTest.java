ed46c99348e738a3109160aa464517515e6f8e40
Denis Renning <denis@devtty.de>
2024-10-04 14:29:15 +0200

    compression ratio

--- src/test/java/org/webcastellum/ZipScannerUtilsTest.java
+++ src/test/java/org/webcastellum/ZipScannerUtilsTest.java
@@ -12,7 +12,7 @@ import static org.junit.Assert.*;
 public class ZipScannerUtilsTest {
     
     @Test
-    public void testExtractNameAndCommentStrings_Stream() throws FileNotFoundException, IOException{
+    public void testExtractNameAndCommentStrings_Stream() throws IOException{
         InputStream input = new FileInputStream(new File("src/test/resources/test.zip"));
         String[] str = ZipScannerUtils.extractNameAndCommentStrings(input);
         Assert.assertArrayEquals(str, new String[]{"test1.txt", "test1comment", "test2.txt"});
@@ -27,13 +27,15 @@ public class ZipScannerUtilsTest {
 
     @Test
     public void testIsZipBombWithNegativeTotalSize(){
-        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () -> ZipScannerUtils.isZipBomb(new File("src/test/resources/test.zip"), -1, 0, 3.0));
+        File file = new File("src/test/resources/test.zip");
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () -> ZipScannerUtils.isZipBomb(file, -1, 0, 3.0));
         assertEquals("thresholdTotalSize must not be negative", ex.getMessage());
     }
     
     @Test
     public void testIsZipBombWithNegativeFileCount(){
-        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () -> ZipScannerUtils.isZipBomb(new File("src/test/resources/test.zip"), 0, -1, 3.0));
+        File file = new File("src/test/resources/test.zip");
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () -> ZipScannerUtils.isZipBomb(file, 0, -1, 3.0));
         assertEquals("thresholdFileCount must not be negative", ex.getMessage());
     }
     
@@ -47,7 +49,7 @@ public class ZipScannerUtilsTest {
     }
     
     @Test
-    public void testIsZipBomb_Stream() throws FileNotFoundException, IOException{
+    public void testIsZipBomb_Stream() throws IOException{
         InputStream input = new FileInputStream(new File("src/test/resources/test.zip"));
         assertFalse(ZipScannerUtils.isZipBomb(input, 14, 2, 3));
     }
