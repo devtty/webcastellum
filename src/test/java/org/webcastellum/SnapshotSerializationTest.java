c80b03eb6b0a752752fa7f748f386088c78041f2
Denis Renning <denis@devtty.de>
2024-10-04 15:34:17 +0200

    Snapshot Test

--- src/test/java/org/webcastellum/SnapshotSerializationTest.java
+++ src/test/java/org/webcastellum/SnapshotSerializationTest.java
@@ -2,7 +2,6 @@ package org.webcastellum;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
