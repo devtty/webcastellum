7318db7a0a7ab29a12b2452742babb6d39f35f9d
Denis Renning <denis@devtty.de>
2024-10-04 14:33:28 +0200

    compression ratio

--- src/test/java/org/webcastellum/ZipScannerUtilsTest.java
+++ src/test/java/org/webcastellum/ZipScannerUtilsTest.java
@@ -2,7 +2,6 @@ package org.webcastellum;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import org.junit.Assert;
